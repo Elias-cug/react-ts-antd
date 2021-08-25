76299e513e0a350e45be57bbf5024353146c2511
liyisheng@bonree.com <liyisheng@bonree.com>
2021-08-25 11:36:19 +0800

    目录结构调整

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Routes from "./router/index";
-import store from './store/index'
+import store from './redux/index'
 
 ReactDOM.render(
   <Provider store={store}>
