484bcbeaad1e615d258284468564b2aa7740d196
liyisheng@bonree.com <liyisheng@bonree.com>
2021-08-31 16:59:25 +0800

    增加antd测试代码

---
+++ src/page/antd-test/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { Select } from 'antd';
+const { Option } = Select;
+
+const Main: FC = () => {
+  return (
+    <div>
+      <span>单选</span>
+    </div>
+  );
+};
+
+export default Main;
