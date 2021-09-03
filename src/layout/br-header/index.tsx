928c3d85b9576f302d97a327c4141b34a298dc50
liyisheng@bonree.com <liyisheng@bonree.com>
2021-09-03 16:31:42 +0800

    增加换肤功能

--- src/layout/br-header/index.tsx
+++ src/layout/br-header/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Header () {
-  return <div className='header'>Hello World, Good Morning! Good Afternoon! Good Night!</div>;
+  let isDefaultTheme = true;
+  function changeTheme () {
+    isDefaultTheme = !isDefaultTheme;
+    const html = document.querySelector('html');
+    let theme = isDefaultTheme ? '' : 'orange';
+    html?.setAttribute('data-theme', theme);
+  }
+  return (
+    <div className='header'>
+      <span className='title'>Hello World, Good Morning! Good Afternoon! Good Night!</span>
+      <div className='theme-change' onClick={changeTheme}>
+        切换皮肤
+      </div>
+    </div>
+  );
 }
 
 export default Header;
