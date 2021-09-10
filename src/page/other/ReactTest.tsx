59b429d7854b417043b63022aac5c5f86dc19414
liyisheng@bonree.com <liyisheng@bonree.com>
2021-09-10 17:45:02 +0800

    feat: 增加测试组件

--- src/page/other/ReactTest.tsx
+++ src/page/other/ReactTest.tsx
@@ -1,28 +1,37 @@
 import React, { FC, useEffect, useState } from 'react';
+import { Button, Card } from 'antd';
 
 interface ChildProps {
-  triggerClick: (event: MouseEvent, data: string) => void;
+  triggerClick: (count: number) => void;
 }
+// 测试子组件事件触发到父组件 --> 子组件
 const Child: FC<ChildProps> = (props: ChildProps) => {
-  function handleChildClick(e) {
-    props.triggerClick(e, 'aa');
-  }
+  console.log('child渲染了');
+  let [count, setCount] = useState(0);
+  const handleChildClick = function () {
+    setCount(count + 1);
+    // props.triggerClick(count);
+  };
+  useEffect(() => {
+    props.triggerClick(count);
+  }, [count]);
   return (
     <>
-      <div onClick={handleChildClick}>你好</div>
+      <Button onClick={handleChildClick}>增加</Button>
     </>
   );
 };
 
+// 测试子组件事件触发到父组件 --> 父组件
 const Parent: FC = () => {
-  const [data, setData] = useState('');
-  function handleTriggerClick(e, data) {
-    setData(data);
-  }
+  const [count, setCount] = useState(0);
+  const handleTriggerClick = function (count: number) {
+    setCount(count);
+  };
   return (
     <>
       <Child triggerClick={handleTriggerClick}></Child>
-      <div>{data}</div>
+      <span>count: {count}</span>
     </>
   );
 };
@@ -30,8 +39,20 @@ const Parent: FC = () => {
 const ReactTest: FC = () => {
   return (
     <>
-      <div>r-子组件的事件触发到父组件：</div>
-      <Parent></Parent>
+      <Card className='card-common mb10'>
+        <h3 className='commom-h3 mb8'>
+          <em className='decorative-block'></em>
+          <span>测试子组件事件触发到父组件: </span>
+        </h3>
+        <Parent></Parent>
+      </Card>
+      <Card className='card-common'>
+        <h3 className='commom-h3 mb8'>
+          <em className='decorative-block'></em>
+          <span>测试父组件调用子组件: </span>
+        </h3>
+        <Parent></Parent>
+      </Card>
     </>
   );
 };
