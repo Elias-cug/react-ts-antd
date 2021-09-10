ac0650882e676d8625d3a642e776785c2bd11f3d
elias_lee <liyisheng0113@163.com>
2021-09-10 16:22:48 +0800

    feat: 增加react组件测试

--- src/page/other/ReactTest.tsx
+++ src/page/other/ReactTest.tsx
@@ -1,9 +1,37 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+
+interface ChildProps {
+  triggerClick: (event: MouseEvent, data: string) => void;
+}
+const Child: FC<ChildProps> = (props: ChildProps) => {
+  function handleChildClick(e) {
+    props.triggerClick(e, 'aa');
+  }
+  return (
+    <>
+      <div onClick={handleChildClick}>你好</div>
+    </>
+  );
+};
+
+const Parent: FC = () => {
+  const [data, setData] = useState('');
+  function handleTriggerClick(e, data) {
+    setData(data);
+  }
+  return (
+    <>
+      <Child triggerClick={handleTriggerClick}></Child>
+      <div>{data}</div>
+    </>
+  );
+};
 
 const ReactTest: FC = () => {
   return (
     <>
-      <div>React功能测试</div>
+      <div>r-子组件的事件触发到父组件：</div>
+      <Parent></Parent>
     </>
   );
 };
