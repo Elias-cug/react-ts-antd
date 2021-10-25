2b8db388fd5edcef93849035dc74fe9c39b91b53
liyisheng@bonree.com <liyisheng@bonree.com>
2021-10-25 14:43:49 +0800

    feat: 增加代码编辑器

--- src/page/other/CodeEditor.tsx
+++ src/page/other/CodeEditor.tsx
@@ -5,18 +5,40 @@ import 'ace-builds/src-noconflict/mode-java';
 import 'ace-builds/src-noconflict/theme-github';
 
 const CodeEditor: FC = () => {
+  const defaultValue = {
+    name: 'liyish',
+    age: '16',
+    info: {
+      name: 'hello'
+    }
+  };
   function onChange (a) {
     console.log(a);
   }
   return (
     <>
-      <div>
+      <div className='code-editor'>
         <AceEditor
+          className='editor-content editor-left'
           mode='java'
           theme='github'
-          onChange={onChange}
           name='UNIQUE_ID_OF_DIV'
+          showGutter={false}
+          showPrintMargin={false}
+          defaultValue={JSON.stringify(defaultValue, null, 2)}
           editorProps={{ $blockScrolling: true }}
+          onChange={onChange}
+        />
+        <AceEditor
+          className='editor-content editor-right'
+          mode='java'
+          theme='github'
+          name='UNIQUE_ID_OF_DIV'
+          showGutter={false}
+          showPrintMargin={false}
+          defaultValue={JSON.stringify(defaultValue, null, 2)}
+          editorProps={{ $blockScrolling: true }}
+          onChange={onChange}
         />
       </div>
     </>
