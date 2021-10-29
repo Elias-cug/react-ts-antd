cc48ede0aa52dce494efe9db4347c62c001c207f
liyisheng@bonree.com <liyisheng@bonree.com>
2021-10-29 16:02:52 +0800

    feat: 增加方案

---
+++ src/page/solution-circultion-scroll/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+const CircultionScroll: FC = () => {
+  return (
+    <>
+      <div>无线滚动列表方案</div>
+    </>
+  );
+};
+export default CircultionScroll;
---
+++ src/page/solution-monitor/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+const FrontMonitor: FC = () => {
+  return (
+    <>
+      <div>前端埋点监控方案</div>
+    </>
+  );
+};
+export default FrontMonitor;
